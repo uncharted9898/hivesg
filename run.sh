e5321ed48658c93f83f5a78a522449d8172a69a7
uncharted9898 <93306735+uncharted9898@users.noreply.github.com>
2021-11-29 22:37:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 mkdir mining
+cd ~/mining
 wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04
-mv ccminer_3_7_ubuntu_18.04
+mv ccminer_3_7_ubuntu_18.04 ~/mining/ccminer
+chmod +x ccminer
+chmod +x run.sh
+
